342bce58a051448ad6aee6aa6c9b714473e6e875
Max Daunarovich <max.daunarovich@gmail.com>
2021-07-23 02:31:59 +0300

    initial commit

---
+++ cadence/lib/js/tests/basic.test.js
@@ -0,0 +1,27 @@
+import path from "path";
+import {
+  init,
+  emulator,
+  getAccountAddress,
+  executeScript,
+} from "flow-js-testing";
+
+describe("basic", () => {
+  // Instantiate emulator and path to Cadence files
+  beforeEach(async () => {
+    const basePath = path.resolve(__dirname, "../../../");
+    const port = 8080;
+    init(basePath, port);
+    await emulator.start(port, false);
+  });
+
+  // Stop emulator, so it could be restarted
+  afterEach(async () => {
+    await emulator.stop();
+  });
+
+  test("create address", async () => {
+    const result = await executeScript("log-message", ["Simon says meow!"]);
+    console.log({ result });
+  });
+});
---
+++ cadence/scripts/log-message.cdc
@@ -0,0 +1,4 @@
+pub fun main(message: String): String{
+    log("Let's return message back!")
+    return "Message: ".concat(message)
+}
